41a9a7c733d29217ac3236157719220214be5f7e
helgasitanggang93 <helga.sitanggang93@gmail.com>
2019-05-24 10:14:48 +0700

    deploy GCP

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 const miniwpRoutes = require('./routes/miniwproutes')
 const cors = require('cors')
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/mini-wp', { useNewUrlParser: true })
+mongoose.connect(`${process.env.MONGO_DB}-miniwp`, { useNewUrlParser: true })
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
